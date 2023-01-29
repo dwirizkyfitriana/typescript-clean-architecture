39ed35948b7d51bb95861b4a5f18a8cc1a72d67b
Dwi Fitriana <cc2039506@gmail.com>
2023-01-30 00:16:51 +0700

    chore: add docker

---
+++ jest-mongodb-config.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+
+module.exports = {
+    mongodbMemoryServerOptions: {
+        binary: {
+            version: '6.0.4',
+            skipMD5: true
+        },
+        autoStart: false,
+        instance: {}
+    }
+}
